2a8df4179e8bec3e7311e18f1ab671d8d9a85147
faisaldp123 <faisal.dpathshala@gmail.com>
2025-06-17 17:19:42 +0530

    fixed the issue

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { adminLogin, adminLogout, checkAdminAuth } = require('../controllers/adminController');
+const { adminLogin, adminLogout, checkAdminAuth, checkAdminSession } = require('../controllers/adminController');
 
 router.post('/login', adminLogin);
 router.post('/logout', adminLogout); // new route
 router.get('/check-auth', checkAdminAuth);
+router.get('/check-admin-session', checkAdminSession);
 
 
 module.exports = router;
